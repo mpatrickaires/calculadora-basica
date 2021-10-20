842386659e991a20ef6f2a215910f0f7aeabfda9
Matheus Patrick Aires Lopes <mpatrickaires@gmail.com>
2021-10-20 18:50:51 -0300

    Definido eventos do teclado e pequenas refatoracoes realizadas

--- index.js
+++ index.js
@@ -2,18 +2,31 @@
 var visor = document.getElementById('visor')
 
 visor.onkeydown = (e) => { 
-    if (!(e.key == 'ArrowLeft' || e.key == 'ArrowRight')) {
+    if (e.key !== 'ArrowLeft' || e.key !== 'ArrowRight') {
         return false
     }
 }
 
+function visorValorNumerico() { 
+    return parseFloat(visor.value)
+}
+
 // Definindo os eventos dos botões de número
+atribuirValorVisor = function(valor) {
+    if (visor.value == '0') { visor.value = '' }
+            
+    if (resultado !== '') {
+        visor.value = ''
+        resultado = ''
+    }
+
+    if (visor.value.length <= 20) { visor.value += valor }
+}
+
 function atribuirEventoElementosNumero(elementos) {
     Array.from(elementos).forEach(elemento => {
         elemento.addEventListener('click', () => {
-            if (visor.value == '0') { visor.value = '' } 
-
-            if (visor.value.length <= 90) { visor.value += elemento.value } 
+            atribuirValorVisor(elemento.value)
         })
     });
 }
@@ -25,30 +38,33 @@ atribuirEventoElementosNumero(elementosNumero)
 // Definindo o evento do botão de limpar
 document.getElementById('limpar').addEventListener('click', () => {
     visor.value = '0'
-    hasPonto = false
     operador = ''
     primeiroValor = ''
 })
 
 // Definindo o evento do botão de ponto (.)
-var hasPonto = false
+function adicionarPonto() {
+    if (!visor.value.includes('.')) { visor.value += '.' }
+}
 
 document.getElementById('ponto').addEventListener('click', () => {
-    if (!hasPonto) { visor.value += '.' }
-    hasPonto = true
+    adicionarPonto()
 })
 
 // Definindo o evento dos botões de operação
 var operador = ''
 var primeiroValor = ''
-var segundoValor = ''
+
+function atribuirOperador(valor) {
+    operador = valor
+    primeiroValor = visorValorNumerico()
+    visor.value = '0'
+}
 
 function atribuirEventoElementosOperador(elementos) {
     Array.from(elementos).forEach(elemento => {
         elemento.addEventListener('click', () => {
-            operador = elemento.value
-            primeiroValor = visor.value
-            visor.value = '0'
+            atribuirOperador(elemento.value)
         })
     })
 }
@@ -57,12 +73,13 @@ var elementosOperador = document.getElementsByClassName('operador')
 atribuirEventoElementosOperador(elementosOperador)
 
 // Definindo o evento do botão de igual (=)
+var segundoValor = ''
 var resultado = ''
 
 document.getElementById('igual').addEventListener('click', () => {
     if (operador.length === 0) { return false }
 
-    segundoValor  = visor.value
+    segundoValor = visorValorNumerico()
 
     switch (operador) {
         case '+': 
@@ -82,8 +99,33 @@ document.getElementById('igual').addEventListener('click', () => {
             break
     }
 
-    primeiroValor = ''
-    segundoValor = ''
-
     visor.value = resultado
+    primeiroValor = segundoValor
 })
+
+// Definindo o evento de utilizar a calculadora através dos botões do teclado
+function isOperador(valor) {
+    switch (valor) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+            return true
+        default:
+            return false
+    }
+}
+
+document.onkeydown = (e) => {
+    var valorTecla = e.key
+
+    if (!isNaN(valorTecla) && !isNaN(parseFloat(valorTecla))) { 
+        atribuirValorVisor(valorTecla)
+    }
+    else if (valorTecla === '.') {
+        adicionarPonto()
+    }
+    else if (isOperador(valorTecla)) {
+        atribuirOperador(valorTecla)
+    }
+}
